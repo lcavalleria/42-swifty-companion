bb9028a9c900ec9a78dd7515cd526ed0c6517131
lcavalleria <llcav92@gmail.com>
2022-09-28 17:37:14 +0200

    companion object for credentials

--- app/src/main/java/com/lcavalle/switfy_companion/Credentials.kt
+++ app/src/main/java/com/lcavalle/switfy_companion/Credentials.kt
@@ -3,13 +3,17 @@ package com.lcavalle.switfy_companion
 /**
  * Contains the credentials for the 42 API.
  * This is just the sample, the credentials won't be uploaded to Git.
- * Changes to this file are ignored by git using "git update-index --assume-unchanged [<file> ...]"
+ * Changes to this file are ignored by git using "git update-index --assume-unchanged <file>"
  * They need to be set manually after cloning the repository.
  * You can find them in 42 intra profile, API section, after the application has been created.
  * Keep in mind that the Secret has a lifetime of 1 month approx.
  */
 
 class Credentials {
-    val UID: String = ""
-    val Secret: String = ""
+    companion object {
+        val UID: String =
+            ""
+        val Secret: String =
+            ""
+    }
 }
